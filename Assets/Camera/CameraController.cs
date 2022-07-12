91299dec3cf0f642af4db59a45b739ab49441a7a
Anthony Diggle <fuseinabowl@gmail.com>
2022-07-12 04:13:43 +0100

    Make the camera draggable by clicking and dragging the mouse

--- Assets/Camera/CameraController.cs
+++ Assets/Camera/CameraController.cs
@@ -6,15 +6,17 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     [SerializeField]
-    private Transform targetTransform = null;
-
-    [SerializeField]
     private float clipWidth = 100f;
     [SerializeField]
     private float clipMiddleDistance = 600f;
     [SerializeField]
     private float viewWidth = 20f;
 
+    [SerializeField]
+    private float dragSpeed = 1f;
+
+    private Vector3 dragClickStartPosition = Vector3.zero;
+    private Vector3 dragCameraStartPosition = Vector3.zero;
     private new Camera camera = null;
 
     private void Awake()
@@ -25,6 +27,7 @@ public class CameraController : MonoBehaviour
     private void Start()
     {
         SetCameraProperties();
+        transform.position = Vector3.up * clipMiddleDistance;
     }
 
     private void SetCameraProperties()
@@ -36,7 +39,23 @@ public class CameraController : MonoBehaviour
 
     private void Update()
     {
-        transform.position = targetTransform.position + Vector3.up * clipMiddleDistance;
         SetCameraProperties();
+ 
+        if (Input.GetMouseButtonDown(0))
+        {
+            dragClickStartPosition = Input.mousePosition;
+            dragCameraStartPosition = transform.position;
+            dragCameraStartPosition.Scale(new Vector3(1f, 0f, 1f));
+            return;
+        }
+ 
+        if (!Input.GetMouseButton(0)) return;
+ 
+        var mousePositionDelta = camera.ScreenToViewportPoint(Input.mousePosition - dragClickStartPosition);
+        Debug.Log($"Position delta: {mousePositionDelta}");
+        mousePositionDelta.Scale(new Vector3(viewWidth * camera.aspect, viewWidth, 0f));
+        var mouseWorldDelta = Grid.Swizzle(GridLayout.CellSwizzle.XZY, mousePositionDelta);
+ 
+        transform.position = dragCameraStartPosition - mouseWorldDelta * dragSpeed + Vector3.up * clipMiddleDistance;
     }
 }
