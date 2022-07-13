4cbcacb150e07fae853dadc05e07c4840a26d699
Anthony Diggle <fuseinabowl@gmail.com>
2022-07-13 14:55:49 +0100

    Add lerp to the delayed draggable object

--- Assets/GameUi/Scripts/ClickableObject.cs
+++ Assets/GameUi/Scripts/ClickableObject.cs
@@ -10,11 +10,15 @@ public class ClickableObject : MonoBehaviour
 
     [SerializeField]
     private float timeUntilTrackMouse = 0.1f;
+    [SerializeField]
+    private float lerpToMouseDuration = 0.1f;
 
     [SerializeField]
     private ClickableObjects clickableObjects = null;
 
     private float clickStartTime;
+    private Vector2 preClickPosition;
+    private Vector2 targetPosition;
 
     private void Awake()
     {
@@ -25,6 +29,7 @@ public class ClickableObject : MonoBehaviour
 
     private void OnEnable()
     {
+        targetPosition = new Vector2(transform.position.x, transform.position.z);
         remoteClickableObject = new ClickableObjects.ClickableObject{
             getCircle = GetCircle,
             onClicked = OnClicked,
@@ -33,8 +38,18 @@ public class ClickableObject : MonoBehaviour
         clickableObjects.clickableObjects.Add(remoteClickableObject);
     }
 
+    private void Update()
+    {
+        var timeSinceClick = Time.unscaledTime - clickStartTime;
+        var timeSinceStartMoving = timeSinceClick - timeUntilTrackMouse;
+        var proportionOfTimeThroughMovement = timeSinceStartMoving / lerpToMouseDuration;
+        var outputPosition = Vector2.Lerp(preClickPosition, targetPosition, proportionOfTimeThroughMovement);
+        transform.position = MousePositionToWorldPosition(outputPosition);
+    }
+
     private void OnDisable()
     {
+        remoteClickableObject.alive = false;
         clickableObjects.clickableObjects.Remove(remoteClickableObject);
         remoteClickableObject = null;
     }
@@ -50,14 +65,27 @@ public class ClickableObject : MonoBehaviour
     private void OnClicked(Vector2 mousePosition)
     {
         clickStartTime = Time.unscaledTime;
+        preClickPosition = new Vector2(transform.position.x, transform.position.z);
+        targetPosition = preClickPosition;
     }
 
     private void OnDragged(Vector2 mousePosition)
     {
         var timeSinceClick = Time.unscaledTime - clickStartTime;
-        if (timeUntilTrackMouse < timeSinceClick)
+        if (timeSinceClick > timeUntilTrackMouse)
         {
-            transform.position = new Vector3(mousePosition.x, 0f, mousePosition.y);
+            targetPosition = mousePosition;
+            if (timeSinceClick > timeUntilTrackMouse + lerpToMouseDuration)
+            {
+                // skip waiting for this class's update, guarantees to avoid one frame of latency
+                preClickPosition = mousePosition;
+                transform.position = MousePositionToWorldPosition(mousePosition);
+            }
         }
+    }
+
+    private Vector3 MousePositionToWorldPosition(Vector2 mousePosition)
+    {
+        return new Vector3(mousePosition.x, 0f, mousePosition.y);
     }
 }
